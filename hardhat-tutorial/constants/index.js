1ab0f86f6715cd43e55f6d5c65eaa421bcffbe68
dadude <dadude@users.noreply.github.com>
2022-10-27 01:12:19 +0200

    add ico code to gh repo ico in prep for vercel

---
+++ hardhat-tutorial/constants/index.js
@@ -0,0 +1,4 @@
+// Address of the NFT Contract that you deployed
+const CRYPTO_DEVS_NFT_CONTRACT_ADDRESS = "0xb3b860D081b07cFAbf11c01813f9BBb2fa842C50";
+
+module.exports = { CRYPTO_DEVS_NFT_CONTRACT_ADDRESS };
